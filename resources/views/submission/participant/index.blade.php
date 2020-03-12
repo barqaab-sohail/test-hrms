bb14517d8714175a9a4722fdf1a5bb71c9a7f05c
Sohail Afzal <sohail.afzal@barqaab.com>
2020-03-12 22:21:40 +0500

    12-102020 2

--- resources/views/submission/participant/index.blade.php
+++ resources/views/submission/participant/index.blade.php
@@ -36,10 +36,21 @@
 		                            	<h3 class="box-title">Submission Participant</h3>
 		                            	</div>
 		                            	<div class="col-md-3">
-		                            	<h4>Total Marks: {{$submission->total_marks}}</h4>
+		                            	<h4>Total Marks: <select  name="total_marks"  class="form-control selectTwo">
+		                                           	<option value=""></option>
+		                                           	<option value="100" {{(old("evaluation_ratio",$submission->total_marks)=="100"? "selected" : "")}}>100</option>
+		                                           	<option value="1000" {{(old("evaluation_ratio",$submission->total_marks)=="1000"? "selected" : "")}}>1000</option>
+		                                       
+		                                           	</select></h4>
 		                            	</div>
 		                            	<div class="col-md-3">
-		                            	<h4>Evaluation Ratio: {{$submission->evaluation_ratio}}</h4>
+		                            	<h4>Evaluation Ratio: <select id="ratio" name="evaluation_ratio"  class="form-control selectTwo">
+		                                           	<option value=""></option>
+		                                           	<option value="80:20" {{(old("evaluation_ratio",$submission->evaluation_ratio)=="80:20"? "selected" : "")}}>80:20</option>
+		                                           	<option value="90:10" {{(old("evaluation_ratio",$submission->evaluation_ratio)=="90:10"? "selected" : "")}}>90:10</option>
+		                                           	<option value="70:30" {{(old("evaluation_ratio",$submission->evaluation_ratio)=="70:30"? "selected" : "")}}>70:30</option>
+		                                           	<option value="Least Cost" {{(old("evaluation_ratio",$submission->evaluation_ratio	)=="Least Cost"? "selected" : "")}}>Least Cost</option>
+		                                           	</select></h4>
 		                            	</div>
 		                            </div>
 		                           
@@ -48,28 +59,22 @@
 		                            <div class="clone" id="clone_1">
 		                            <!--row 1 -->
 		                            <div class="row">
-		                                <div class="col-md-12">
+		                                <div class="col-md-8">
 		                                 <!--/span 1-1 -->
 		                                    <div class="form-group row">
 		                                        <div class="col-md-12">
 		                                       		<label class="control-label text-right">Name of Participant</label><br>
-		                                       		<input type="text" name="participant_name" value="{{ old('participant_name.0') }}"  class="form-control participant_name"  >
+		                                       		<input type="text" name="participant_name" data-validation="required" value="{{ old('participant_name.0') }}"  class="form-control participant_name"  >
 		                                        </div>
 		                                    </div>
 		                                </div>
-		                                  
-		                            </div>
-		                            
-		                            
-		                              <!--row 3-->
-		                            <div class="row">
 		                                <!--/span 3-1 -->
 		                                <div class="col-md-2">
 		                                    <div class="form-group row">
 		                                        <div class="col-md-12">
-		                                        	<label class="control-label text-right">Technical Score</span></label>
+		                                        	<label class="control-label text-right">Technical Point</span></label>
 		                                        
-		                                            <input type="text"   name="technical_score[]"  value="{{ old('technical_score.0') }}" class="form-control technical_score">
+		                                            <input type="text"   data-validation="number" data-validation-optional="true" data-validation-allowing="range[1;{{$submission->total_marks}}],float"  data-validation-error-msg="value must be less than Total Marks" name="technical_point[]"  value="{{ old('technical_point.0') }}" class="form-control technical_point">
 													 
 		                                        </div>
 		                                    </div>
@@ -80,11 +85,27 @@
 		                                    <div class="form-group row">
 		                                        <div class="col-md-12">
 		                                        	<label class="control-label text-right">Financial Cost</label>
-		                                       		<input type="text"  name="financial_cost[]" value="{{ old('financial_cost.0') }}"  class="form-control financial_cost">
+		                                       		<input type="text" data-validation="number" data-validation-optional="true" data-validation-allowing="range[1;10000000000],float"  data-validation-error-msg="value must be Numeric"  name="financial_cost[]" value="{{ old('financial_cost.0') }}"  class="form-control financial_cost">
 		                                        </div>
 		                                    </div>
 		                                </div>
+		                                  
+		                            </div>
+		                            
+		                            
+		                              <!--row 3-->
+		                            <div class="row">
+		                                
 		                                <!--/span 3-2 -->
+		                                <div class="col-md-2">
+		                                    <div class="form-group row">
+		                                        <div class="col-md-12">
+		                                        	<label class="control-label text-right">Technical Score</label>
+		                                       		<input type="text" name="technical_score[]" value="{{ old('technical_score.0') }}"  class="form-control technical_score" readonly>
+		                                        </div>
+		                                    </div>
+		                                </div>
+		                                 <!--/span 3-2 -->
 		                                <div class="col-md-2">
 		                                    <div class="form-group row">
 		                                        <div class="col-md-12">
@@ -112,7 +133,7 @@
 		                                    </div>
 		                                </div>
 
-		                                <div class="col-md-2">
+		                                <div class="col-md-4">
 		                                    
 		                                			<div class="float-right">
 		                                			<br>
@@ -122,7 +143,7 @@
 										</div>
 		                                
 		                            </div>
-
+		                             <hr class="m-t-0 m-b-40">
 		                            </div>
 
 		                        
@@ -195,10 +216,13 @@
 		 $(this).closest(".clone").remove();
  		}); 
 	
+
+
 	
 		//Calculate Score
 
 		 $("#calculate").click(function(){
+		 	$(":input[type=text][readonly='readonly']").val("");
 
 		 			//calculate Lowest Financial Cost
 				 	var financial_array =[];
@@ -207,21 +231,35 @@
 						financial_array.push(financial_cost);
 					});
 					var lowest = Math.min.apply(Math,financial_array);
+			
+			//get last two digit of evaluation_ratio
+			var financial_ratio = parseInt($('#ratio').val().slice(-2));
+
+			//get last two digit of evaluation_ratio
+			var technical_ratio = parseInt($('#ratio').val().substring(0, 2));
+
+			
 
 			var overall_array =[];
 		 	$(".clone").each(function(){
-		 		var technical_score = parseInt($(this).find(".technical_score").val());
+		 		var technical_point = parseInt($(this).find(".technical_point").val());
+
 		 		var financial_cost = parseInt($(this).find(".financial_cost").val());
-		 		var ratio = "{{$submission->evaluation_ratio}}"
 		 		
-		 		if(ratio == "80:20"){
-		 			var financial_score = parseFloat((lowest/financial_cost*20).toFixed(3));	
+		 		
+		 		if($.isNumeric(financial_ratio)){
+		 			var financial_score = parseFloat((lowest/financial_cost*financial_ratio).toFixed(3));	
 		 			$(this).find(".financial_score").val(financial_score);
+		 		
+		 			var technical_score = parseFloat((technical_point*technical_ratio/100).toFixed(3));	
+		 			$(this).find(".technical_score").val(technical_score);
 		 		}
 		 	
 		 		
 		 		//Overall Score
+		 		if($.isNumeric(financial_ratio)){
 		 		var overall_cost = $(this).find(".overall_score").val(financial_score+technical_score);
+		 		}
 		 		
 
 		 		var name= $(this).find(".participant_name").val();
@@ -229,28 +267,26 @@
 		 	
 		 		overall_array.push({
 		 			overall: overall,
+		 			financil:financial_cost,
 		 			name: name
 		 		});
 		 	});
 
+		 	if($.isNumeric(financial_ratio)){
 		 	overall_array.sort(function(a,b) {return (a.overall < b.overall) ? 1 : ((b.overall < a.overall) ? -1 : 0);} );
+		 	}else{
+		 		overall_array.sort(function(a,b) {return (a.financil > b.financil) ? 1 : ((b.financil > a.financil) ? -1 : 0);} );
+		 	}
 		 
 		 	$.each(overall_array, function (index, value){
 		 		$(".clone").each(function(){
 		 			if ( $(this).find(".participant_name").val() == value['name'] ){
-		 				$(this).find(".ranking").val('Rank-'+index+1);
+		 				$(this).find(".ranking").val('Rank-'+(index+1));
 		 			}
 		 		//alert(value['name']+": Rank" +value['overall']+":"+index);
 		 		});
 		 	})
-		 	//Enter Raking
 		 	
-		 			// 	var i;
-						// for (i = 0; i < overall_array.length; ++i) {
-    		// 				if (name == overall_array[i]['name']){
-    		// 					alert(name);
-    		// 				}
-						// }
 		 
 			
 
